36beee425e3de5f0bad9f5a7042e326756e28c4a
telebotu <retrovintagecart@gmail.com>
2024-06-11 01:21:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ apt update && apt upgrade -y
 apt install git -y           
 pip install -U pip
 
-git clone https://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@github.com/yaduhhhh/SptKidungamaniBot kidu                     
+git clone https://github.com/telebotu/johnns1 kidu                     
 cd kidu
 pip install -U -r requirements.txt
 python3 bot.py
